71f4d4f54f9187fcf22d41876f69b4d5de97f91d
Svetlana Eroshkina <eroshkinavenni1998@yandex.ru>
2026-03-11 22:15:50 +0300

    Добавлен файл .gitignore, исправлены аннотации методов, все вызовы к driver описаны в BasePage

--- tests/test_faq.py
+++ tests/test_faq.py
@@ -8,9 +8,10 @@ from pages.main_page import MainPage
 class TestFAQAnswers:
     @allure.title("Тест ответов на вопросы")
     @pytest.mark.parametrize('question_id, expected_answer', data.FAQData.faq_answers)
-    def test_faq_answers(self, driver, accept_cookies, question_id, expected_answer):
+    def test_faq_answers(self, driver, question_id, expected_answer):
         # Arrange
         main_page = MainPage(driver)
+        main_page.accept_cookies()
         main_page.click_faq_question(question_id)
         # Act
         actual_answer = main_page.get_faq_answer_text(question_id)
